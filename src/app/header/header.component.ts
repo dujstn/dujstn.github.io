3a26e6b638173e3da297b89c1ea260995e27c133
Justin Du <du.justinz@gmail.com>
2024-12-31 01:27:23 -0800

    added mobile nav drawer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  title="Justin Du";
+  public screenWidth!: number;
+
+  private drawerIsOpen: boolean = false;
+
+  title: string = 'Justin Du';
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  changeNavDrawer() {
+    this.drawerIsOpen = !this.drawerIsOpen;
+  }
+
+  updateScreenWidth(width: number) {
+    this.screenWidth = width;
+  }
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number) {
+    this.updateScreenWidth(width);
+  }
+
+  isMobileWidth() {
+    return this.screenWidth <= 650;
+  }
+
+  isDrawerOpen() {
+    return this.drawerIsOpen;
+  }
 }
